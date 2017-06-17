872887087a7e46ae7254542c26d6099faa234a76
carlos <carlosroberto.rojas@gmail.com>
2017-06-16 20:24:46 -0500

    se agregaron operaciones matematicas

--- Untitled-1.java
+++ Untitled-1.java
@@ -2,6 +2,12 @@ Public class hello {
  public static void main (String [] args ){
      Sytem.out.println("Hola Mundo");
 
+     int a = 67;
+     int d = 45;
+     int c = a+b;
+
+     Sytem.out.println("Adios");
+
  }
 
 
